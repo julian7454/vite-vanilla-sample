e6def2324a3e95deffd75b3fbbcac09e034fbd0e
julian7454 <julian7454@gmail.com>
2023-04-12 11:42:36 +0800

    build: Add README and husky

---
+++ README.md
@@ -0,0 +1,18 @@
+# Project
+
+## install
+yarn
+
+## development
+yarn dev
+
+## build
+yarn build
+
+## coding style
+yarn lint
+
+## language
+預設不拆分語系檔，可選擇是否要拆分出語系檔案：
+- 在 src/js/i18n/index.js 設定改用 i18next-http-backend
+- 個別語系的設定檔在 src/locales，會在 build 的過程打包至 dist
--- src/js/main.js
+++ src/js/main.js
@@ -11,43 +11,39 @@ gsap.registerPlugin(ScrollTrigger);
 
 import "./i18n";
 
-$(function () {
 
-    const langCode = {
-        中文: "cn",
-        en: "en",
-    };
+const langCode = {
+    中文: "cn",
+    en: "en",
+};
 
-    const $langItem = $(".lang .lang__item");
+const $langItem = $(".lang .lang__item");
 
-    $(".lang-select-menu button").click(function () {
-        const code = langCode[$(this).html().toLowerCase()];
-        const selectText = $(this).html();
-        $langItem.removeClass("select");
-
-        $langItem.each(function () {
-            if ($(this).html() == selectText) {
-                $(this).addClass("select");
-            }
-        });
-        localStorage.setItem("lang", code);
-        $("body")
-            .removeClass("cn en")
-            .addClass(localStorage.getItem("lang") || "cn");
-
-            i18next.changeLanguage(code, function(err, t) {
-                // 翻譯整個頁面
-                $('body').localize();
-            });
-    });
+$(".lang-select-menu button").click(function () {
+    const code = langCode[$(this).html().toLowerCase()];
+    const selectText = $(this).html();
+    $langItem.removeClass("select");
 
     $langItem.each(function () {
-        $(this).removeClass("select");
-        const code = langCode[$(this).html().toLowerCase()];
-        if (code === (localStorage.getItem("lang") || "cn")) {
+        if ($(this).html() == selectText) {
             $(this).addClass("select");
         }
     });
+    localStorage.setItem("lang", code);
+    $("body")
+        .removeClass("cn en")
+        .addClass(localStorage.getItem("lang") || "cn");
 
+        i18next.changeLanguage(code, function(err, t) {
+            // 翻譯整個頁面
+            $('body').localize();
+        });
+});
 
+$langItem.each(function () {
+    $(this).removeClass("select");
+    const code = langCode[$(this).html().toLowerCase()];
+    if (code === (localStorage.getItem("lang") || "cn")) {
+        $(this).addClass("select");
+    }
 });
